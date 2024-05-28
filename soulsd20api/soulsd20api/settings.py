1d016ac4eb7dfea25b06727cd2484be6132f112a
Jordan Watt <JordanAlexWatt@hotmail.com>
2024-05-28 15:23:39 -0700

    Add CORS package whitelisting localhost

--- soulsd20api/soulsd20api/settings.py
+++ soulsd20api/soulsd20api/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'compendium.apps.CompendiumConfig'
+    'compendium.apps.CompendiumConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +137,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost:3000",
+]
